75179c2a2557f302c6ebffcfb54b73e997839135
camille-granier <granierc@protonmail.com>
2022-04-15 14:44:12 +1000

    hamburger menu animation

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,9 @@
 const btnHamburger = document.querySelector('#btnHamburger');
 
 btnHamburger.addEventListener('click', function(){
-    console.log('open');
+    if(btnHamburger.classList.contains('open')) {
+        btnHamburger.classList.remove('open')
+    } else {
+        btnHamburger.classList.add('open')
+    }
 });
