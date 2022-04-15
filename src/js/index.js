c833e3bbc3dd95c39be2b0aa2ba2b498137690de
camille-granier <granierc@protonmail.com>
2022-04-15 14:58:37 +1000

    hamburger cross on open

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@ const btnHamburger = document.querySelector('#btnHamburger');
 btnHamburger.addEventListener('click', function(){
     if(btnHamburger.classList.contains('open')) {
         btnHamburger.classList.remove('open')
+        console.log('close')
     } else {
         btnHamburger.classList.add('open')
     }
